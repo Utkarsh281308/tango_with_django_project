001b965d7beaaf10eb801ab227e2e1303667f6c8
Utkarsh Agrawal <2837124a@student.gla.ac.uk>
2024-02-02 21:38:49 +0000

    Chapter 5 Completed

--- populate_rango.py
+++ populate_rango.py
@@ -38,12 +38,14 @@ def populate():
         'url':'http://flask.pocoo.org'}
     ]
 
-    cats = {'Python': {'pages': python_pages},
-        'Django': {'pages': django_pages},
-        'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'views':128, 'likes':64},
+        'Django': {'pages': django_pages, 'views':64, 'likes':32},
+        'Other Frameworks': {'pages': other_pages, 'views':32, 'likes':16} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        views = cat_data.get('views',0)
+        likes = cat_data.get('likes',0)
+        c = add_cat(cat, views, likes)
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -58,8 +60,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name, views, likes):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
