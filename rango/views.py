23463b91eea3907ba75520c11596d42f3844ab46
Utkarsh Agrawal <2837124a@student.gla.ac.uk>
2024-02-02 17:26:52 +0000

    Chapter 4 Completed

--- rango/views.py
+++ rango/views.py
@@ -7,13 +7,15 @@ def index(request):
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!' }
 
+
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     #return render(request, 'rango/index.html', context=context_dict)
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
-    
+    return render(request, 'rango/index.html', context=context_dict)
+
     
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context = {'newmessage': 'This tutorial has been put together by Utkarsh Agrawal' }
+    return render(request, 'rango/about.html', context=context)
