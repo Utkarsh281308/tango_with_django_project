93bbbcaaf79008652acfe31c4d832f4237b181ab
Utkarsh Agrawal <2837124a@student.gla.ac.uk>
2024-02-02 16:09:46 +0000

    Chapter 3 Completed

--- rango/models.py
+++ rango/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    views = 0
+    likes = 0
+
     class Meta:
         verbose_name_plural = 'Categories'
     def __str__(self):
